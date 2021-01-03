507947083dd8a6b55e00a0fbcd9ad4454471186c
tjefferson1913 <tnwestbrook29@gmail.com>
2021-01-03 13:33:53 -0500

    Testing time

--- src/script.js
+++ src/script.js
@@ -35,7 +35,7 @@ function formatDate(date) {
 
   function formatHours(timestamp) {
     //let date =new Date(timestamp);
-    let hours = date.getHours();
+    /*let hours = date.getHours();
     if (hours < 10) {
       hours =`0${hours}`;
     }
@@ -43,7 +43,7 @@ function formatDate(date) {
     let minutes = date.getMinutes();
     if (minutes < 10) {
       minutes =`0${minutes}`;
-    }
+    }*/
    
     return `${hours}:${minutes}`;
 
